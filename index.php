a93d12166d2655effba2c8ada7a615b887ad67e5
Hoang Vo <hoang6777@icloud.com>
2024-02-23 00:50:26 -0700

    updated the login and register page

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +13,30 @@
 <body>
     <div class="container">
         <div class="box form-box">
+            <?php
+
+                include("config.php");
+                if(isset($_PORT['submit'])){
+                    $email = mysqli_real_escape_string($con, $_POST['email']);
+                    $password = mysqli_real_escape_string($con,$_POST['password']);
+                    $result = mysqli_query($con, "SELECT * FROM users WHERE Email='$email' AND Password='$password'") or die("Select Error");
+                    $row = mysqli_fetch_assoc($result);
+                    if(is_array($row) && !empty($row)){
+                        $_SESSION['valid'] = $row['Email'];
+                        $_SESSION['username'] = $row['Username'];
+                        $_SESSION['score'] = $row['Score'];
+                        $_SESSION['id'] = $row['Id'];
+                    }else{
+                        echo "<div class='message'>
+                            <p>Wrong Username or Password</p>
+                        </div> <br>";
+                        echo "<a href='index.php'><button class='btn'>Go Back</button>";
+                    }
+                    if(isset($_SESSION['valid'])){
+                        header("Location: home.php");
+                    }
+                }else{
+            ?>
             <header>Login</header>
             <form action="" method="post">
                 <div class="field input">
@@ -28,6 +55,7 @@
                 </div>
             </form>
         </div>
+        <?php }?>
     </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <title>Register</title>
+    <title>Sign Up</title>
 </head>
 <body>
     <div class="container">
@@ -51,7 +51,7 @@
                     <input type="password" name="password" id="password" autocomplete="off" required>
                 </div>
                 <div class="field">
-                    <input type="submit" class="btn" name="submit" value="Sign up" required>
+                    <input type="submit" class="btn" name="submit" value="Register" required>
                 </div>
                 <div class="links">
                     Already a member? <a href="index.php">Sign In</a>
