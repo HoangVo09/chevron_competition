097321a14ea2a4c183732d32e70a948671f8b3ee
Hoang Vo <hoang6777@icloud.com>
2024-02-23 00:22:47 -0700

    more bug fixed

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-    $con = mysqli_connect("localhost","root@localhost","","chevron_competition") or die("couldn't connect");
+    $con = mysqli_connect("localhost","root","","chevron_competition") or die("couldn't connect");
 ?>
