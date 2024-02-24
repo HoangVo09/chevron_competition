7c6356c653e9f057dca706ca4a4e5afc896d4094
Hoang Vo <hoang6777@icloud.com>
2024-02-23 17:32:44 -0700

    nav bar

--- home.php
+++ home.php
@@ -60,6 +60,13 @@
             </p>
         </section>
     </main>
+    <div class="vertical-menu">
+        <a href="#" class="active">Home</a>
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+        <a href="#">Link 4</a>
+    </div>
 
     <footer class="footer">
         <div class="container">
